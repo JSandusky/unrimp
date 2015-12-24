28fd75d5e68c4d1adea2ee3d78ae074167d0b0e8
Christian Ofenberg <cofenberg@gmail.com>
2015-12-24 10:00:03 +0100

    Runtime: Material property kickoff
    Former-commit-id: 383edbb171086fd38c805f95b28916243ea7b93a
    Former-commit-id: 402745b279b6da8be7589f11cf6fc8b455d083ea

---
+++ Renderer/RendererRuntime/include/RendererRuntime/Resource/MaterialBlueprint/MaterialBlueprintProperty.h
@@ -0,0 +1,91 @@
+/*********************************************************\
+ * Copyright (c) 2012-2015 Christian Ofenberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+\*********************************************************/
+
+
+//[-------------------------------------------------------]
+//[ Header guard                                          ]
+//[-------------------------------------------------------]
+#pragma once
+
+
+//[-------------------------------------------------------]
+//[ Includes                                              ]
+//[-------------------------------------------------------]
+#include "RendererRuntime/Resource/Material/MaterialProperty.h"
+
+
+//[-------------------------------------------------------]
+//[ Namespace                                             ]
+//[-------------------------------------------------------]
+namespace RendererRuntime
+{
+
+
+	//[-------------------------------------------------------]
+	//[ Classes                                               ]
+	//[-------------------------------------------------------]
+	/**
+	*  @brief
+	*    Material blueprint property
+	*/
+	class MaterialBlueprintProperty : public MaterialProperty
+	{
+
+
+	//[-------------------------------------------------------]
+	//[ Public methods                                        ]
+	//[-------------------------------------------------------]
+	public:
+		/**
+		*  @brief
+		*    Constructor
+		*
+		*  @param[in] materialPropertyId
+		*    Material property ID
+		*/
+		explicit MaterialBlueprintProperty(MaterialPropertyId materialPropertyId);
+
+		/**
+		*  @brief
+		*    Destructor
+		*/
+		inline ~MaterialBlueprintProperty();
+
+
+	//[-------------------------------------------------------]
+	//[ Private methods                                       ]
+	//[-------------------------------------------------------]
+	private:
+		MaterialBlueprintProperty(const MaterialBlueprintProperty&) = delete;
+		MaterialBlueprintProperty& operator=(const MaterialBlueprintProperty&) = delete;
+
+
+	};
+
+
+//[-------------------------------------------------------]
+//[ Namespace                                             ]
+//[-------------------------------------------------------]
+} // RendererRuntime
+
+
+//[-------------------------------------------------------]
+//[ Implementation                                        ]
+//[-------------------------------------------------------]
+#include "RendererRuntime/Resource/MaterialBlueprint/MaterialBlueprintProperty.inl"
