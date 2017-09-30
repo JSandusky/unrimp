c4f1ebfc0b6f03dd55b2975473cbc87351e15c3b
Christian Ofenberg <cofenberg@gmail.com>
2017-09-30 09:41:47 +0200

    Renderer runtime: "RendererRuntime::Math": "MAGIC_PRIME" now comes from http://www.isthe.com/chongo/tech/comp/fnv/ (32 bit value was already used in "RendererRuntime::StringId", which is using FNV-1a as well)

--- Renderer/RendererRuntime/src/Core/Math/Math.cpp
+++ Renderer/RendererRuntime/src/Core/Math/Math.cpp
@@ -124,8 +124,8 @@ namespace RendererRuntime
 
 	uint32_t Math::calculateFNV1a32(const uint8_t* content, uint32_t numberOfBytes, uint32_t hash)
 	{
-		// 32-bit FNV-1a implementation basing on http://de.wikipedia.org/wiki/FNV_%28Informatik%29
-		static const uint32_t MAGIC_PRIME = 2166136261u;
+		// 32-bit FNV-1a implementation basing on http://www.isthe.com/chongo/tech/comp/fnv/
+		static const uint32_t MAGIC_PRIME = 16777619u;
 		const uint8_t* end = content + numberOfBytes;
 		for ( ; content < end; ++content)
 		{
@@ -136,8 +136,8 @@ namespace RendererRuntime
 
 	uint64_t Math::calculateFNV1a64(const uint8_t* content, uint32_t numberOfBytes, uint64_t hash)
 	{
-		// 64-bit FNV-1a implementation basing on http://de.wikipedia.org/wiki/FNV_%28Informatik%29
-		static const uint64_t MAGIC_PRIME = 0x00000100000001b3u;
+		// 64-bit FNV-1a implementation basing on http://www.isthe.com/chongo/tech/comp/fnv/
+		static const uint64_t MAGIC_PRIME = 1099511628211u;
 		const uint8_t* end = content + numberOfBytes;
 		for ( ; content < end; ++content)
 		{
