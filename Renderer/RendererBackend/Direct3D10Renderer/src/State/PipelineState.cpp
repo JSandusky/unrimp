afc3b5d911b06bf1234eb5344fb2d7c62d532195
Christian Ofenberg <cofenberg@gmail.com>
2018-02-03 09:21:08 +0100

    Direct3D 10 and 11 renderer backend: Attribute-less rendering: Resolved "D3D11 WARNING: ID3D11Device::CreateInputLayout: An Input Layout is being created with 0 elements.  This is valid to do, perhaps as a degenerate case. This warning is for the author to verify whether intent was to have more than 0 elements. [ STATE_CREATION WARNING #420: CREATEINPUTLAYOUT_EMPTY_LAYOUT]"

--- Renderer/RendererBackend/Direct3D10Renderer/src/State/PipelineState.cpp
+++ Renderer/RendererBackend/Direct3D10Renderer/src/State/PipelineState.cpp
@@ -62,52 +62,55 @@ namespace Direct3D10Renderer
 		mProgram->addReference();
 		mRenderPass->addReference();
 
-		// Create Direct3D 10 input element descriptions
-		const VertexShaderHlsl* vertexShaderHlsl = static_cast<ProgramHlsl*>(mProgram)->getVertexShaderHlsl();
-		if (nullptr != vertexShaderHlsl)
+		// Create Direct3D 10 input element descriptions with support for attribute-less rendering
+		const uint32_t numberOfAttributes = pipelineState.vertexAttributes.numberOfAttributes;
+		if (numberOfAttributes > 0)
 		{
-			const uint32_t numberOfAttributes = pipelineState.vertexAttributes.numberOfAttributes;
-			const Renderer::VertexAttribute* attributes = pipelineState.vertexAttributes.attributes;
-
-			// Create Direct3D 10 input element descriptions
-			// TODO(co) We could manage in here without new/delete when using a fixed maximum supported number of elements
-			const Renderer::Context& context = getRenderer().getContext();
-			D3D10_INPUT_ELEMENT_DESC* d3d10InputElementDescs   = numberOfAttributes ? RENDERER_MALLOC_TYPED(context, D3D10_INPUT_ELEMENT_DESC, numberOfAttributes) : RENDERER_MALLOC_TYPED(context, D3D10_INPUT_ELEMENT_DESC, 1);
-			D3D10_INPUT_ELEMENT_DESC* d3d10InputElementDesc    = d3d10InputElementDescs;
-			D3D10_INPUT_ELEMENT_DESC* d3d10InputElementDescEnd = d3d10InputElementDescs + numberOfAttributes;
-			for (; d3d10InputElementDesc < d3d10InputElementDescEnd; ++d3d10InputElementDesc, ++attributes)
+			const VertexShaderHlsl* vertexShaderHlsl = static_cast<ProgramHlsl*>(mProgram)->getVertexShaderHlsl();
+			if (nullptr != vertexShaderHlsl)
 			{
-				// Fill the "D3D10_INPUT_ELEMENT_DESC"-content
-				d3d10InputElementDesc->SemanticName      = attributes->semanticName;																	// Semantic name (LPCSTR)
-				d3d10InputElementDesc->SemanticIndex     = attributes->semanticIndex;																	// Semantic index (UINT)
-				d3d10InputElementDesc->Format            = static_cast<DXGI_FORMAT>(Mapping::getDirect3D10Format(attributes->vertexAttributeFormat));	// Format (DXGI_FORMAT)
-				d3d10InputElementDesc->InputSlot         = static_cast<UINT>(attributes->inputSlot);													// Input slot (UINT)
-				d3d10InputElementDesc->AlignedByteOffset = attributes->alignedByteOffset;																// Aligned byte offset (UINT)
+				const Renderer::VertexAttribute* attributes = pipelineState.vertexAttributes.attributes;
 
-				// Per-instance instead of per-vertex?
-				if (attributes->instancesPerElement > 0)
+				// Create Direct3D 10 input element descriptions
+				// TODO(co) We could manage in here without new/delete when using a fixed maximum supported number of elements
+				const Renderer::Context& context = getRenderer().getContext();
+				D3D10_INPUT_ELEMENT_DESC* d3d10InputElementDescs   = numberOfAttributes ? RENDERER_MALLOC_TYPED(context, D3D10_INPUT_ELEMENT_DESC, numberOfAttributes) : RENDERER_MALLOC_TYPED(context, D3D10_INPUT_ELEMENT_DESC, 1);
+				D3D10_INPUT_ELEMENT_DESC* d3d10InputElementDesc    = d3d10InputElementDescs;
+				D3D10_INPUT_ELEMENT_DESC* d3d10InputElementDescEnd = d3d10InputElementDescs + numberOfAttributes;
+				for (; d3d10InputElementDesc < d3d10InputElementDescEnd; ++d3d10InputElementDesc, ++attributes)
 				{
-					d3d10InputElementDesc->InputSlotClass       = D3D10_INPUT_PER_INSTANCE_DATA;	// Input classification (D3D10_INPUT_CLASSIFICATION)
-					d3d10InputElementDesc->InstanceDataStepRate = attributes->instancesPerElement;	// Instance data step rate (UINT)
-				}
-				else
-				{
-					d3d10InputElementDesc->InputSlotClass       = D3D10_INPUT_PER_VERTEX_DATA;	// Input classification (D3D10_INPUT_CLASSIFICATION)
-					d3d10InputElementDesc->InstanceDataStepRate = 0;							// Instance data step rate (UINT)
-				}
-			}
+					// Fill the "D3D10_INPUT_ELEMENT_DESC"-content
+					d3d10InputElementDesc->SemanticName      = attributes->semanticName;																	// Semantic name (LPCSTR)
+					d3d10InputElementDesc->SemanticIndex     = attributes->semanticIndex;																	// Semantic index (UINT)
+					d3d10InputElementDesc->Format            = static_cast<DXGI_FORMAT>(Mapping::getDirect3D10Format(attributes->vertexAttributeFormat));	// Format (DXGI_FORMAT)
+					d3d10InputElementDesc->InputSlot         = static_cast<UINT>(attributes->inputSlot);													// Input slot (UINT)
+					d3d10InputElementDesc->AlignedByteOffset = attributes->alignedByteOffset;																// Aligned byte offset (UINT)
 
-			{ // Create the Direct3D 10 input layout
-				ID3DBlob* d3dBlobVertexShader = vertexShaderHlsl->getD3DBlobVertexShader();
-				direct3D10Renderer.getD3D10Device()->CreateInputLayout(d3d10InputElementDescs, numberOfAttributes, d3dBlobVertexShader->GetBufferPointer(), d3dBlobVertexShader->GetBufferSize(), &mD3D10InputLayout);
-			}
+					// Per-instance instead of per-vertex?
+					if (attributes->instancesPerElement > 0)
+					{
+						d3d10InputElementDesc->InputSlotClass       = D3D10_INPUT_PER_INSTANCE_DATA;	// Input classification (D3D10_INPUT_CLASSIFICATION)
+						d3d10InputElementDesc->InstanceDataStepRate = attributes->instancesPerElement;	// Instance data step rate (UINT)
+					}
+					else
+					{
+						d3d10InputElementDesc->InputSlotClass       = D3D10_INPUT_PER_VERTEX_DATA;	// Input classification (D3D10_INPUT_CLASSIFICATION)
+						d3d10InputElementDesc->InstanceDataStepRate = 0;							// Instance data step rate (UINT)
+					}
+				}
 
-			// Destroy Direct3D 10 input element descriptions
-			RENDERER_FREE(context, d3d10InputElementDescs);
-		}
-		else
-		{
-			RENDERER_LOG(direct3D10Renderer.getContext(), CRITICAL, "Failed to create the Direct3D 10 pipeline stage input layout because there's no vertex shader")
+				{ // Create the Direct3D 10 input layout
+					ID3DBlob* d3dBlobVertexShader = vertexShaderHlsl->getD3DBlobVertexShader();
+					direct3D10Renderer.getD3D10Device()->CreateInputLayout(d3d10InputElementDescs, numberOfAttributes, d3dBlobVertexShader->GetBufferPointer(), d3dBlobVertexShader->GetBufferSize(), &mD3D10InputLayout);
+				}
+
+				// Destroy Direct3D 10 input element descriptions
+				RENDERER_FREE(context, d3d10InputElementDescs);
+			}
+			else
+			{
+				RENDERER_LOG(direct3D10Renderer.getContext(), CRITICAL, "Failed to create the Direct3D 10 pipeline stage input layout because there's no vertex shader")
+			}
 		}
 
 		// Assign a default name to the resource for debugging purposes
@@ -135,7 +138,10 @@ namespace Direct3D10Renderer
 	void PipelineState::bindPipelineState() const
 	{
 		// Set the Direct3D 10 input layout
-		mD3D10Device->IASetInputLayout(mD3D10InputLayout);
+		if (nullptr != mD3D10InputLayout)
+		{
+			mD3D10Device->IASetInputLayout(mD3D10InputLayout);
+		}
 
 		// Set the program
 		static_cast<Direct3D10Renderer&>(getRenderer()).setProgram(mProgram);
--- Renderer/RendererBackend/Direct3D11Renderer/src/State/PipelineState.cpp
+++ Renderer/RendererBackend/Direct3D11Renderer/src/State/PipelineState.cpp
@@ -62,53 +62,56 @@ namespace Direct3D11Renderer
 		mProgram->addReference();
 		mRenderPass->addReference();
 
-		// Create Direct3D 11 input element descriptions
-		const VertexShaderHlsl* vertexShaderHlsl = static_cast<ProgramHlsl*>(mProgram)->getVertexShaderHlsl();
-		if (nullptr != vertexShaderHlsl)
+		// Create Direct3D 11 input element descriptions with support for attribute-less rendering
+		const uint32_t numberOfAttributes = pipelineState.vertexAttributes.numberOfAttributes;
+		if (numberOfAttributes > 0)
 		{
-			ID3DBlob* d3dBlobVertexShader = vertexShaderHlsl->getD3DBlobVertexShader();
-			if (nullptr != d3dBlobVertexShader)
+			const VertexShaderHlsl* vertexShaderHlsl = static_cast<ProgramHlsl*>(mProgram)->getVertexShaderHlsl();
+			if (nullptr != vertexShaderHlsl)
 			{
-				const uint32_t numberOfAttributes = pipelineState.vertexAttributes.numberOfAttributes;
-				const Renderer::VertexAttribute* attributes = pipelineState.vertexAttributes.attributes;
-
-				// TODO(co) We could manage in here without new/delete when using a fixed maximum supported number of elements
-				const Renderer::Context& context = direct3D11Renderer.getContext();
-				D3D11_INPUT_ELEMENT_DESC* d3d11InputElementDescs   = numberOfAttributes ? RENDERER_MALLOC_TYPED(context, D3D11_INPUT_ELEMENT_DESC, numberOfAttributes) : RENDERER_MALLOC_TYPED(context, D3D11_INPUT_ELEMENT_DESC, 1);
-				D3D11_INPUT_ELEMENT_DESC* d3d11InputElementDesc    = d3d11InputElementDescs;
-				D3D11_INPUT_ELEMENT_DESC* d3d11InputElementDescEnd = d3d11InputElementDescs + numberOfAttributes;
-				for (; d3d11InputElementDesc < d3d11InputElementDescEnd; ++d3d11InputElementDesc, ++attributes)
+				ID3DBlob* d3dBlobVertexShader = vertexShaderHlsl->getD3DBlobVertexShader();
+				if (nullptr != d3dBlobVertexShader)
 				{
-					// Fill the "D3D11_INPUT_ELEMENT_DESC"-content
-					d3d11InputElementDesc->SemanticName      = attributes->semanticName;																	// Semantic name (LPCSTR)
-					d3d11InputElementDesc->SemanticIndex     = attributes->semanticIndex;																	// Semantic index (UINT)
-					d3d11InputElementDesc->Format            = static_cast<DXGI_FORMAT>(Mapping::getDirect3D11Format(attributes->vertexAttributeFormat));	// Format (DXGI_FORMAT)
-					d3d11InputElementDesc->InputSlot         = static_cast<UINT>(attributes->inputSlot);													// Input slot (UINT)
-					d3d11InputElementDesc->AlignedByteOffset = attributes->alignedByteOffset;																// Aligned byte offset (UINT)
+					const Renderer::VertexAttribute* attributes = pipelineState.vertexAttributes.attributes;
 
-					// Per-instance instead of per-vertex?
-					if (attributes->instancesPerElement > 0)
+					// TODO(co) We could manage in here without new/delete when using a fixed maximum supported number of elements
+					const Renderer::Context& context = direct3D11Renderer.getContext();
+					D3D11_INPUT_ELEMENT_DESC* d3d11InputElementDescs   = numberOfAttributes ? RENDERER_MALLOC_TYPED(context, D3D11_INPUT_ELEMENT_DESC, numberOfAttributes) : RENDERER_MALLOC_TYPED(context, D3D11_INPUT_ELEMENT_DESC, 1);
+					D3D11_INPUT_ELEMENT_DESC* d3d11InputElementDesc    = d3d11InputElementDescs;
+					D3D11_INPUT_ELEMENT_DESC* d3d11InputElementDescEnd = d3d11InputElementDescs + numberOfAttributes;
+					for (; d3d11InputElementDesc < d3d11InputElementDescEnd; ++d3d11InputElementDesc, ++attributes)
 					{
-						d3d11InputElementDesc->InputSlotClass       = D3D11_INPUT_PER_INSTANCE_DATA;	// Input classification (D3D11_INPUT_CLASSIFICATION)
-						d3d11InputElementDesc->InstanceDataStepRate = attributes->instancesPerElement;	// Instance data step rate (UINT)
+						// Fill the "D3D11_INPUT_ELEMENT_DESC"-content
+						d3d11InputElementDesc->SemanticName      = attributes->semanticName;																	// Semantic name (LPCSTR)
+						d3d11InputElementDesc->SemanticIndex     = attributes->semanticIndex;																	// Semantic index (UINT)
+						d3d11InputElementDesc->Format            = static_cast<DXGI_FORMAT>(Mapping::getDirect3D11Format(attributes->vertexAttributeFormat));	// Format (DXGI_FORMAT)
+						d3d11InputElementDesc->InputSlot         = static_cast<UINT>(attributes->inputSlot);													// Input slot (UINT)
+						d3d11InputElementDesc->AlignedByteOffset = attributes->alignedByteOffset;																// Aligned byte offset (UINT)
+
+						// Per-instance instead of per-vertex?
+						if (attributes->instancesPerElement > 0)
+						{
+							d3d11InputElementDesc->InputSlotClass       = D3D11_INPUT_PER_INSTANCE_DATA;	// Input classification (D3D11_INPUT_CLASSIFICATION)
+							d3d11InputElementDesc->InstanceDataStepRate = attributes->instancesPerElement;	// Instance data step rate (UINT)
+						}
+						else
+						{
+							d3d11InputElementDesc->InputSlotClass       = D3D11_INPUT_PER_VERTEX_DATA;	// Input classification (D3D11_INPUT_CLASSIFICATION)
+							d3d11InputElementDesc->InstanceDataStepRate = 0;							// Instance data step rate (UINT)
+						}
 					}
-					else
-					{
-						d3d11InputElementDesc->InputSlotClass       = D3D11_INPUT_PER_VERTEX_DATA;	// Input classification (D3D11_INPUT_CLASSIFICATION)
-						d3d11InputElementDesc->InstanceDataStepRate = 0;							// Instance data step rate (UINT)
-					}
+
+					// Create the Direct3D 11 input layout
+					direct3D11Renderer.getD3D11Device()->CreateInputLayout(d3d11InputElementDescs, numberOfAttributes, d3dBlobVertexShader->GetBufferPointer(), d3dBlobVertexShader->GetBufferSize(), &mD3D11InputLayout);
+
+					// Destroy Direct3D 11 input element descriptions
+					RENDERER_FREE(context, d3d11InputElementDescs);
 				}
-
-				// Create the Direct3D 11 input layout
-				direct3D11Renderer.getD3D11Device()->CreateInputLayout(d3d11InputElementDescs, numberOfAttributes, d3dBlobVertexShader->GetBufferPointer(), d3dBlobVertexShader->GetBufferSize(), &mD3D11InputLayout);
-
-				// Destroy Direct3D 11 input element descriptions
-				RENDERER_FREE(context, d3d11InputElementDescs);
 			}
-		}
-		else
-		{
-			RENDERER_LOG(direct3D11Renderer.getContext(), CRITICAL, "Failed to create the Direct3D 11 pipeline stage input layout because there's no vertex shader")
+			else
+			{
+				RENDERER_LOG(direct3D11Renderer.getContext(), CRITICAL, "Failed to create the Direct3D 11 pipeline stage input layout because there's no vertex shader")
+			}
 		}
 
 		// Assign a default name to the resource for debugging purposes
@@ -136,7 +139,10 @@ namespace Direct3D11Renderer
 	void PipelineState::bindPipelineState() const
 	{
 		// Set the Direct3D 11 input layout
-		mD3D11DeviceContext->IASetInputLayout(mD3D11InputLayout);
+		if (nullptr != mD3D11InputLayout)
+		{
+			mD3D11DeviceContext->IASetInputLayout(mD3D11InputLayout);
+		}
 
 		// Set the program
 		static_cast<Direct3D11Renderer&>(getRenderer()).setProgram(mProgram);
